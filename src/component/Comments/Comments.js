e21b6cd349ad3e8f02b22a955701dc1b037752d7
AndrewVillalba <andrewvillalba1@gmail.com>
2023-04-12 22:20:42 -0400

    finished styling for tablet

--- src/component/Comments/Comments.js
+++ src/component/Comments/Comments.js
@@ -6,13 +6,17 @@ import CommentCard from '../CommentCard/CommentCard.js'
 function Comments({videos}){
     return (
         <div className='comment-section'>
+            <p className='comment-section__number'>3 Comments</p>
             <div className='input'>
                 <img className='input-img' src={mohan}></img>
                 <div className='input-wrapper'>
                     <form className='input-wrapper'>
-                        <label className='input-wrapper__label' htmlFor='text'>JOIN THE CONVERSATION</label>
-                        <textarea className='input-wrapper__text' placeholder='Add a new comment' required name='text'></textarea>
-                        <label htmlFor='submit' ></label>
+                        <div className='text-wrapper'>
+                            <label className='input-wrapper__label' htmlFor='text'>JOIN THE CONVERSATION</label>
+                            <textarea className='input-wrapper__text' placeholder='Add a new comment' required name='text'></textarea>
+                            <label htmlFor='submit' ></label>
+                        </div>
+                        
                         <button name='submit' className='input-wrapper__submit'>COMMENT</button>
                     </form>
                 </div>   
