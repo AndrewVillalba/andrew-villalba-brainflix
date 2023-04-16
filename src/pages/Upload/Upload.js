a21deedde5f669f7c0ef64b6ea60dc254a2faaee
AndrewVillalba <andrewvillalba1@gmail.com>
2023-04-16 17:34:24 -0400

    finished upload styling

--- src/pages/Upload/Upload.js
+++ src/pages/Upload/Upload.js
@@ -8,17 +8,21 @@ function Upload() {
         <div className='upload-wrapper'>
             <div className='upload'>
                 <h1 className='upload__heading'>Upload Video</h1>
-                <p className='upload-label__img'>VIDEO THUMBNAIL</p>
-                <img className='upload__img' src={uploadPreview}></img>
-                <form className='upload-form'>
-                    <label for='title' className='upload-label__title'>TITLE YOUR VIDEO</label>
-                    <textarea placeholder='Add a title to your video' name='title' className='upload-form__title'></textarea>
-                    <label for='desc' className='upload-label__desc'>ADD A VIDEO DESCRIPTION</label>
-                    <textarea placeholder='Add a description to your video' name='desc' className='upload-form__desc'></textarea>
-                </form>
+                <div className='desktop-wrapper'>
+                    <div className='upload__img-wrapper'>
+                       <p className='upload-label__img'>VIDEO THUMBNAIL</p>
+                        <img className='upload__img' src={uploadPreview}></img> 
+                    </div>
+                    <form className='upload-form'>
+                        <label for='title' className='upload-label__title'>TITLE YOUR VIDEO</label>
+                        <textarea placeholder='Add a title to your video' name='title' className='upload-form__title'></textarea>
+                        <label for='desc' className='upload-label__desc'>ADD A VIDEO DESCRIPTION</label>
+                        <textarea placeholder='Add a description to your video' name='desc' className='upload-form__desc'></textarea>
+                    </form>
+                </div>
                 <div className='upload-buttons'>
-                    <Link to={'/'}><button className='upload-buttons__publish'>PUBLISH</button></Link>
-                    <button className='upload-buttons__cancel'>CANCEL</button> 
+                    <Link to={'/'}><button className='upload-buttons__publish buttons'>PUBLISH</button></Link>
+                    <button className='upload-buttons__cancel buttons'>CANCEL</button> 
                 </div>
             </div>
         </div>
